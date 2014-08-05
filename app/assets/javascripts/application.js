1331f62b1c4d9ca20349ff5a76ca949982b7a12a
Vu Bui <vbui@nmr.mpibpc.mpg.de>
2014-08-05 14:03:47 +0200

    phylo tree interacts with graphical view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -92,3 +92,26 @@ function update_data_center_table(str_map) {
     this.innerText = map[$(this).attr('data')];
   });
 }
+
+/*
+ * .addClassSVG(className)
+ * Adds the specified class(es) to each of the set of matched SVG elements.
+ */
+$.fn.addClassSVG = function(className){
+    $(this).attr('class', function(index, existingClassNames) {
+        return existingClassNames + ' ' + className;
+    });
+    return this;
+};
+
+/*
+ * .removeClassSVG(className)
+ * Removes the specified class to each of the set of matched SVG elements.
+ */
+$.fn.removeClassSVG = function(className){
+    $(this).attr('class', function(index, existingClassNames) {
+        var re = new RegExp(className, 'g');
+        return existingClassNames.replace(re, '');
+    });
+    return this;
+};
