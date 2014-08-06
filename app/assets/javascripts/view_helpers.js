8670aaae0b13d1065f4b1db3437cd90621378663
Vu Bui <vbui@nmr.mpibpc.mpg.de>
2014-08-06 14:33:42 +0200

    remove unused code. implement manual species mapping.

--- app/assets/javascripts/view_helpers.js
+++ app/assets/javascripts/view_helpers.js
@@ -34,31 +34,6 @@ function getSelectedItems(itemList) {
 }
 
 /**
- * Adds a species mapping block.
- */
-function addSpeciesMappingBlock() {
-  var $btn = $(this),
-    $currentSpeciesMappingBlock = $btn.prev();
-
-  $newBlock = $currentSpeciesMappingBlock.clone();
-  new_seq_names = $newBlock.find('ul input');
-  for (var i = 0; i < new_seq_names.length; i++) {
-    if ( $(new_seq_names[i]).is(':checked') ) {
-      $(new_seq_names[i]).parent().remove();
-    }
-  }
-
-  new_seq_names.click(function() {
-    disable($btn, false);
-    if (getSelectedItems(new_seq_names).length == new_seq_names.length)  {
-      disable($btn, true);
-    }
-  });
-
-  $newBlock.insertAfter($currentSpeciesMappingBlock);
-}
-
-/**
  * Checks all input.
  * @param inputs an array of checkbox input elements.
  */
@@ -67,52 +42,5 @@ function checkAll(inputs, checkOrNot) {
     inputs.prop('checked', 'checked');
   } else {
     inputs.removeAttr('checked');
-  }
-}
-
-/**
- * Returns columns with at least x % amount of introns
- */
-function intronCols(xPercent) {
-  var $textBasedTable = $('table#text_based_output');
-
-  var numberOfGenes = $textBasedTable.find('tr').length,
-    allCells = $textBasedTable.find('td').toArray(),
-    charPerRow = allCells.length / numberOfGenes,
-    numberOfCommonIntrons = Math.round(xPercent*(numberOfGenes - 1)/100);
-
-  var cols = [],
-    allCols = [];
-
-  for (var i = 0; i < charPerRow; i++) {
-    allCols.push(allCells.filter(function(value, index) {
-      return (index - i) % charPerRow == 0;
-    }));
-  }
-
-  for (var i = 1; i < charPerRow; i++) {
-    if (numberOfIntrons(allCols[i]) >= numberOfCommonIntrons) {
-      cols.push(allCols[i]);
-    }
-  }
-
-  return cols;
-}
-
-function highlightIntrons(xPercent, styleOrNot) {
-  var $textBasedTable = $('table#text_based_output');
-
-  var numberOfGenes = $textBasedTable.find('tr').length,
-    allCells = $textBasedTable.find('td').toArray(),
-    charPerRow = allCells.length / numberOfGenes;
-
-  var cols = intronCols(xPercent);
-
-  for (var i = 0; i < cols.length; i++) {
-    if (styleOrNot) {
-      colorIntronColumn(cols[i], styleOrNot);
-    } else {
-      colorIntronColumn(cols[i], styleOrNot);
-    }
   }
 }
--- app/helpers/gene_painter_helper.rb
+++ app/helpers/gene_painter_helper.rb
@@ -2,33 +2,6 @@ require 'genestructures.rb'
 
 module GenePainterHelper
 
-  def add_species(seq_names)
-    space = "margin-right: 20px"
-
-    seq_names_list = ""
-    seq_names.each do |seq_name|
-      seq_names_list << content_tag(:li,
-        check_box_tag("seq_name", seq_name, false, :disabled => false) +
-        # check_box_tag("seq_name", seq_name, false, :disabled => true) +
-        "&nbsp;&nbsp;".html_safe +
-        content_tag(:span, seq_name))
-    end
-
-    content_tag(:div,
-      text_field_tag("species",
-        nil,
-        :placeholder => "Input species",
-        :style => space
-      ) +
-      content_tag(:div,
-        content_tag(:ul,
-          seq_names_list.html_safe
-        ),
-        :style => space + "; width: 150px; height: 150px; overflow: auto; display:inline-block"
-      )
-    )
-  end
-
   def create_data_center_table(seq_names)
     table_body = ''
 
