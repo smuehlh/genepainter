0162a896c6e54cc433af2db3cb945953972763dd
Stefanie Mühlhausen <smuehlh@fab8.(none)>
2014-12-16 11:27:04 +0100

    Enabled template error again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
- # 	rescue_from ActionView::Template::Error do |exception|
-	# 	render "error", formats: [:js]
-	# end
+ 	rescue_from ActionView::Template::Error do |exception|
+		render "error", formats: [:js]
+	end
 end
