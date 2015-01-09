eab95b6749fa646e32feb35a67769a865a221cd4
Stefanie Mühlhausen <smuehlh@fab8.(none)>
2015-01-09 14:31:29 +0100

    Depreciated Add/removeClassSVG methods. Using classList instead.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -210,30 +210,6 @@ function create_alignment_file_ajax() {
   });
 }
 
-
-/*
- * .addClassSVG(className)
- * Adds the specified class(es) to each of the set of matched SVG elements.
- */
-$.fn.addClassSVG = function(className){
-    $(this).attr('class', function(index, existingClassNames) {
-        return existingClassNames + ' ' + className;
-    });
-    return this;
-};
-
-/*
- * .removeClassSVG(className)
- * Removes the specified class to each of the set of matched SVG elements.
- */
-$.fn.removeClassSVG = function(className){
-    $(this).attr('class', function(index, existingClassNames) {
-        var re = new RegExp(className, 'g');
-        return existingClassNames.replace(re, '');
-    });
-    return this;
-};
-
 $.fn.sc_ExecuteAjaxQ = function (options) {
   //? Executes a series of AJAX methods in dequence
 
