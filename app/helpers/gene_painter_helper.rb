cdeab9ddb39387f06440adba9e54501d7fb8241d
Stefanie Mühlhausen <smuehlh@fab8.(none)>
2015-01-21 14:01:07 +0100

    Classes for patterns (like text-based, merged, stats) set in ruby helper, not javascript

--- app/helpers/gene_painter_helper.rb
+++ app/helpers/gene_painter_helper.rb
@@ -145,7 +145,7 @@ module GenePainterHelper
     return sequence_names
   end
 
-  def get_statistics_table(filename, id_patterns_table)
+  def old_get_statistics_table(filename, id_patterns_table)
 
     names_table, pattern_table, stats_table, stats_table_head, intronpos_table = [], [], [], [], []
 
@@ -185,7 +185,7 @@ module GenePainterHelper
         else
           # "normal pattern" line
           names_table << "<tr><td>#{name}</td></tr>"
-          pattern_table << pattern_to_tr(striped_pattern) # important: use pattern without white-spaces       
+          pattern_table << old_pattern_to_tr(striped_pattern) # important: use pattern without white-spaces       
         end
 
       else
@@ -233,34 +233,61 @@ module GenePainterHelper
       intronpos_table.join.html_safe
   end
 
+  def get_statistics_table(filename, id_pattern_table, id_intronpos_table)
+    pattern_data, names_data, intronnum_data = [], [], []
+    stats_data, stats_head = [], []
+    introns_per_column = nil # init with nil value, 
+
+    IO.foreach(filename) do |line|
+      line = line.chomp
+      next if line.empty?
+
+      if line[0] == ">" then 
+        # pattern, treat as in get_table method
+        introns_per_column = init_intron_counts(line) if introns_per_column.nil?
+        update_wanted_data(line, pattern_data, names_data, introns_per_column)
+      else
+        # statistics
+        update_stats(line, stats_data, stats_head)
+      end
+    end
+    intronpos_data = intronpos_to_pattern(introns_per_column)
+    pattern_data.unshift intronpos_data
+    names_data.unshift ">Intron number"
+
+    pattern_table = build_pattern_table(pattern_data, introns_per_column, 
+      {id: id_pattern_table, classes: 'with_border', is_no_col_classes: true} 
+    )
+    names_table = build_table(names_data)
+    intronpos_table = build_intronpos_table(intronpos_data, {id: id_intronpos_table, classes: 'with_border'})
+
+    stats_only_th_table = build_stats_table(stats_head, true) # true: use th tag
+    stats_only_td_table = build_stats_table(stats_data, false) # false: use tr tag
+
+    return pattern_table, names_table, intronpos_table, stats_only_th_table, stats_only_td_table
+  end
+
   def get_table(filename, opts={})
-    table_head_str = "<table"
-    table_head_str << " class='#{opts[:table_class]}'" if opts[:table_class]
-    table_head_str << " id='#{opts[:table_id]}'" if opts[:table_id]
-    table_head_str << ">"
-    table = [ table_head_str ]
+    is_return_merged_table = opts[:is_merged_table]
+    id_pattern_table = opts[:id_pattern_table] || ""
 
-    is_first_line = true
+    pattern_data, names_data = [], []
+    introns_per_column = nil # important: init with nil value, will be replaced by array of pattern-size
 
-    File.open(filename, "r").each_line do |line|
-
-      name = line[0...GeneAlignment.max_length_gene_name].strip
-      if name =~ /^>Merged/ || name =~ /^>Consensus/ then
-        # this is a special pattern, do not include it in text-based output!
-        next
-      end
-
-      pattern = line[GeneAlignment.max_length_gene_name..-1].strip
-      if is_first_line && opts[:colgroup_class] then
-        # get table row and generate colgroup
-        table << pattern_to_colgroup( pattern, opts[:colgroup_class] )
-        is_first_line = false
-      end
-      table << pattern_to_tr(pattern)
+    IO.foreach(filename) do |line|
+      introns_per_column = init_intron_counts(line) if introns_per_column.nil?
+      update_wanted_data(line, pattern_data, names_data, introns_per_column)
     end
 
-    table << "</table>"
-    return table.join.html_safe
+    pattern_table = build_pattern_table(pattern_data, introns_per_column, {id: id_pattern_table})
+    names_table = build_table(names_data)
+    if is_return_merged_table then 
+      id_merged_table = opts[:id_merged_table] || "merged-unused-id"
+      merged_table = build_merged_pattern_table(introns_per_column, id_merged_table)
+      return pattern_table, names_table, merged_table
+    else
+      return pattern_table, names_table
+    end
   end
 
   def copy_alignment_for_lucullus(file_src)
@@ -353,39 +380,175 @@ module GenePainterHelper
   end
 
   ### helper methods for converting exon-intron pattern to table rows
-  def pattern_to_tr(pattern)
-    data = ["<tr>"]
-    pattern.each_char do |char|
-      data.push "<td>#{char}</td>"
+  def split_pattern_line(line)
+    name = line[0...GeneAlignment.max_length_gene_name].strip
+    pattern = line[GeneAlignment.max_length_gene_name..-1].strip
+    pattern = pattern.gsub(" ", "") # only statistics-pattern contains spaces, but filtering them out does not hurt!
+    pattern = pattern.chars # split into array
+    return pattern, name
+  end
+  def update_wanted_data(line, pattern_data, names_data, introns_per_column)
+    pattern, name = split_pattern_line(line) # pattern is an array, name a string!
+    if ! is_unwanted_data(name) then 
+      collect_data(pattern, name, pattern_data, names_data)
+      update_intron_counts(pattern, introns_per_column)
     end
-    data.push "</tr>"
-    return data
+  end
+  def init_intron_counts(line)
+    pattern, name = split_pattern_line(line)
+    return Array.new(pattern.size, 0)
+  end
+  def is_unwanted_data(name)
+    # special patterns should not be included in output!
+    return name == (">Merged") || name.start_with?(">Consensus") || name == (">Intron")
+  end
+  def update_intron_counts(pattern, introns_per_column)
+      pattern.each_with_index do |char, ind|
+        if char != "-" then 
+          # not an exon
+          introns_per_column[ind] += 1
+        end
+      end
+  end
+  def collect_data(pattern, name, pattern_data, names_data)
+    pattern_data.push pattern
+    names_data.push name
   end
 
-  def pattern_to_colgroup(pattern, colgroup_class)
-    data = ["<colgroup class='#{colgroup_class}'>"]
-    pattern.each_char do |char|
-      data.push "<col/>"
-    end
-    data.push "</colgroup>"
-    return data
-  end
-  def merged_pattern_to_intronpos(pattern, opts={})
-    n_introns = 1
-    data = []
-
-    data.push "<tr>"
-    pattern.each_char do |char|
-      if char == "-" then
-        # exon
-        data.push "<td>&nbsp;</td>"
+  def intronpos_to_pattern(introns_per_column)
+    intronpos = 0
+    return introns_per_column.collect do |n_introns|
+      if n_introns == 0 then 
+        dat = "&nbsp;"
       else
-        data.push "<td>#{n_introns}</td>"
-        n_introns += 1
+        dat = intronpos
+        intronpos += 1
+      end
+      dat
+    end
+  end
+
+  def update_stats(line, stats_data, stats_head)
+    if stats_head.empty? then 
+      # first line -> table head
+      stats_head.push line.split("\t")
+    else
+      # any other line -> table data
+      stats_data.push line.split("\t")
+    end
+  end
+  def build_stats_table(data, is_th_tag)
+    table = [ "<table>", nil, "</table>" ]
+    intronpos = 0
+    table[1] = data.collect do |arr|
+      if is_th_tag then 
+        dat = stats_to_tr(arr, "", is_th_tag) # true      
+      else
+        dat = stats_to_tr(arr, class_intron_col(intronpos), is_th_tag) 
+        intronpos += 1
+      end
+      dat
+    end  
+    return table.join.html_safe
+  end
+
+
+  def build_pattern_table(data, additional_info, opts={})
+    open_tag = "<table"
+    open_tag += " id='#{opts[:id]}'" if opts[:id] && opts[:id] != ""
+    open_tag += " class='#{opts[:classes]}'" if opts[:classes] && opts[:classes] != ""
+    open_tag += ">"
+    table = [ open_tag, nil, "</table>" ]
+    table[1] = data.collect do |datum|
+      pattern_to_tr(datum, additional_info, opts[:is_no_col_classes])
+    end
+    return table.join.html_safe
+  end
+  def build_table(data, opts={})
+    open_tag = "<table"
+    open_tag += " id='#{opts[:id]}'" if opts[:id] && opts[:id] != ""
+    open_tag += " class='#{opts[:classes]}'" if opts[:classes] && opts[:classes] != ""
+    open_tag += ">"
+    table = [ open_tag, nil, "</table>" ]
+    table[1] = data.collect do |datum|
+      array_to_tr( [datum] ) 
+    end
+    return table.join.html_safe
+  end
+  def build_merged_pattern_table(introns_per_column, id)
+    pattern = introns_per_column.collect do |n_introns|
+      if n_introns == 0 then 
+        # exon
+        str = "-"
+      else
+        # intron
+        str = "|"
       end
     end
 
-    data.push "</tr>"
-    return data
+    table = ["<table id='#{id}'>", merged_pattern_to_tr(pattern, introns_per_column), "</table>"]
+    return table.join.html_safe
   end
+  def build_intronpos_table(pattern, opts={})
+    open_tag = "<table"
+    open_tag += " id='#{opts[:id]}'" if opts[:id] && opts[:id] != ""
+    open_tag += " class='#{opts[:classes]}'" if opts[:classes] && opts[:classes] != ""
+    open_tag += ">"
+    table = [open_tag, array_to_tr(pattern), "</table>"]
+    return table.join.html_safe
+  end
+  def merged_pattern_to_tr(pattern, introns_per_column)
+    res = ["<tr>", nil, "</tr>"]
+    res[1] = pattern.each_with_index.collect do |char, ind|
+      n_introns = introns_per_column[ind]
+      "<td class='#{n_introns}'>#{char}</td>"
+    end
+    return res.join("")   
+  end
+  def pattern_to_tr(pattern, introns_per_column, is_no_col_classes)
+    res = ["<tr>", nil, "</tr>"]
+    intronpos = 0 # first intron has class intron_0 !
+    res[1] = pattern.each_with_index.collect do |char, ind|
+      n_introns = introns_per_column[ind]
+      if is_no_col_classes then 
+        classes = ""
+      else 
+        classes = "col-#{n_introns}"
+      end
+      if n_introns > 0 then 
+        # intron position in any pattern, not neccessarily this pattern
+        classes += " #{class_intron_col(intronpos)}"
+        intronpos += 1
+      end
+      "<td class='#{classes}'>#{char}</td>"
+    end
+    return res.join("")
+  end
+  # each array element gets td
+  def array_to_tr(arr)
+    res = ["<tr><td>", nil, "</td></tr>"]
+    res[1] = arr.join("</td><td>")
+    return res.join("")
+  end
+  def stats_to_tr(arr, tr_class, is_use_th)
+    res = ["<tr class='#{tr_class}'>", nil, "</tr>"]
+    res[1] = arr.each_with_index.collect do |txt, ind|
+      if ind == 0 then 
+        classes = "stats-intron-count-col"
+      elsif ind == 1
+        classes = "stats-intron-num-col"
+      elsif ind == 2
+        classes = "stats-taxon-col"
+      else
+        classes = ""      
+      end
+      if is_use_th then 
+        "<th class='#{classes}'>#{txt}</th>"
+      else
+        "<td class='#{classes}'>#{txt}</td>"
+      end
+    end
+    return res.join("")
+  end
+
 end
